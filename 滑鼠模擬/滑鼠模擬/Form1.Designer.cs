74c84f151f4036d2ecba5858d3858dd8ae00c017
leon123858 <a0970785699@gmail.com>
2021-06-09 19:59:24 +0800

    mouse API

---
+++ 滑鼠模擬/滑鼠模擬/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace 滑鼠模擬
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.click = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mouse_y = new System.Windows.Forms.TextBox();
+            this.mouse_x = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.script = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // click
+            // 
+            this.click.BackColor = System.Drawing.SystemColors.Control;
+            this.click.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.click.Font = new System.Drawing.Font("標楷體", 20F);
+            this.click.Location = new System.Drawing.Point(14, 223);
+            this.click.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.click.Name = "click";
+            this.click.Size = new System.Drawing.Size(507, 72);
+            this.click.TabIndex = 1;
+            this.click.TabStop = false;
+            this.click.Text = "虛擬左右鍵 開始";
+            this.click.UseVisualStyleBackColor = false;
+            this.click.Click += new System.EventHandler(this.Click_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(124, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "方向上:滑鼠中鍵點擊";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(116, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "左鍵點擊";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label3.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(291, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "右鍵點擊";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(124, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(308, 30);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "方向下:滑鼠左鍵壓下";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label5.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label5.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label5.Location = new System.Drawing.Point(26, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(495, 30);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "(只需輕點一下,用方想左一下解除)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label6.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(72, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(390, 30);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "W,A,S,D 滑鼠移動(目前關)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.textBox1.Location = new System.Drawing.Point(244, 379);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 43);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "50";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label7.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(26, 379);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(215, 30);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "滑鼠移動速度:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button1.Location = new System.Drawing.Point(439, 379);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "confirm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // mouse_y
+            // 
+            this.mouse_y.Font = new System.Drawing.Font("新細明體", 15F);
+            this.mouse_y.Location = new System.Drawing.Point(726, 70);
+            this.mouse_y.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mouse_y.Name = "mouse_y";
+            this.mouse_y.Size = new System.Drawing.Size(107, 43);
+            this.mouse_y.TabIndex = 11;
+            this.mouse_y.Text = "50";
+            // 
+            // mouse_x
+            // 
+            this.mouse_x.Font = new System.Drawing.Font("新細明體", 15F);
+            this.mouse_x.Location = new System.Drawing.Point(604, 70);
+            this.mouse_x.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mouse_x.Name = "mouse_x";
+            this.mouse_x.Size = new System.Drawing.Size(81, 43);
+            this.mouse_x.TabIndex = 12;
+            this.mouse_x.Text = "50";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label8.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(640, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(137, 30);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "滑鼠座標";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label9.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(691, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 30);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Y";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label10.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label10.Font = new System.Drawing.Font("標楷體", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label10.Location = new System.Drawing.Point(569, 70);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 30);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "X";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // script
+            // 
+            this.script.Location = new System.Drawing.Point(555, 158);
+            this.script.Multiline = true;
+            this.script.Name = "script";
+            this.script.Size = new System.Drawing.Size(320, 264);
+            this.script.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button2.Location = new System.Drawing.Point(586, 116);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 35);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "run script";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(887, 430);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.script);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.mouse_x);
+            this.Controls.Add(this.mouse_y);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.click);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "虛擬左右鍵";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button click;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox mouse_y;
+        private System.Windows.Forms.TextBox mouse_x;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox script;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 滑鼠模擬/滑鼠模擬/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+namespace 滑鼠模擬
+{
+    public partial class Form1 : Form
+    {
+        const int WH_KEYBOARD_LL = 13;
+        static int global_X;
+        static int global_Y;
+        static int mouse_velocity;
+        enum MouseEventFlag : uint
+        {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            XDown = 0x0080,
+            XUp = 0x0100,
+            Wheel = 0x0800,
+            VirtualDesk = 0x4000,
+            Absolute = 0x8000
+        }
+        public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
+        static int m_HookHandle = 0;
+        HookProc m_KbdHookProc;
+        // 安装钩子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+        // 卸载钩子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+        // 继续下一个钩子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+        //
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll")] //替換成所需的DLL檔
+        static extern bool SetCursorPos(int X, int Y);
+        [DllImport("user32.dll")]
+        static extern void mouse_event(MouseEventFlag flag, int dx, int dy, uint data, UIntPtr extraInfo);
+        public Form1()
+        {
+            InitializeComponent();
+            global_X = System.Windows.Forms.Cursor.Position.X;
+            global_Y = System.Windows.Forms.Cursor.Position.Y;
+            mouse_velocity = int.Parse(textBox1.Text);
+        }
+        private void Click_Click(object sender, EventArgs e)
+        {
+            if (m_HookHandle == 0)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                {
+                    using (ProcessModule curmodule = curProcess.MainModule)
+                    {
+                        m_KbdHookProc = new HookProc(Form1.hookproc);
+                        m_HookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, m_KbdHookProc, GetModuleHandle(curmodule.ModuleName), 0);
+                    }
+                }
+                if (m_HookHandle == 0)
+                {
+                    MessageBox.Show(" HOOK 失敗!");
+                    return;
+                }
+                MessageBox.Show("建立Hook成功");
+                click.Text = "虛擬左右鍵 停止";
+            }
+            else
+            {
+                bool ret = UnhookWindowsHookEx(m_HookHandle);
+                if (ret == false)
+                {
+                    MessageBox.Show("關閉 Hook 失敗!");
+                    return;
+                }
+                m_HookHandle = 0;
+                MessageBox.Show("解除Hook成功");
+                click.Text = "虛擬左右鍵 開始";
+            }
+        }
+        
+        static int hookproc(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            global_X = System.Windows.Forms.Cursor.Position.X;
+            global_Y = System.Windows.Forms.Cursor.Position.Y;
+            const int delay = 200;
+            // 當按鍵按下及鬆開時都會觸發此函式，這裡只處理鍵盤按下的情形。
+            bool isPressed = (lParam.ToInt32() & 0x80000000) == 0;
+            if (nCode < 0 || !isPressed)
+            {
+               return CallNextHookEx(m_HookHandle, nCode, wParam, lParam);
+            }
+            // 取得欲攔截之按鍵狀態
+            KeyStateInfo LKey = KeyboardInfo.GetKeyState(Keys.Left);
+            KeyStateInfo RKey = KeyboardInfo.GetKeyState(Keys.Right);
+            KeyStateInfo MKey = KeyboardInfo.GetKeyState(Keys.Up);
+            KeyStateInfo DKey = KeyboardInfo.GetKeyState(Keys.Down);
+            KeyStateInfo WKey = KeyboardInfo.GetKeyState(Keys.W);
+            KeyStateInfo AKey = KeyboardInfo.GetKeyState(Keys.A);
+            KeyStateInfo SKey = KeyboardInfo.GetKeyState(Keys.S);
+            KeyStateInfo DLKey = KeyboardInfo.GetKeyState(Keys.D);
+            if (LKey.IsPressed)
+            {
+                Thread.Sleep(delay);
+                mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+                mouse_event(MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+            }
+            if (RKey.IsPressed)
+            {
+                Thread.Sleep(delay);
+                mouse_event(MouseEventFlag.RightDown, 0, 0, 0, UIntPtr.Zero);
+                mouse_event(MouseEventFlag.RightUp, 0, 0, 0, UIntPtr.Zero);
+            }
+            if (MKey.IsPressed)
+            {
+                Thread.Sleep(delay);
+                mouse_event(MouseEventFlag.MiddleDown, 0, 0, 0, UIntPtr.Zero);
+                mouse_event(MouseEventFlag.MiddleUp, 0, 0, 0, UIntPtr.Zero);
+            }
+            if (DKey.IsPressed)
+            {
+                Thread.Sleep(2*delay);
+                mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+            }
+            
+            
+                //if (WKey.IsPressed)
+                //{
+                //    SetCursorPos(global_X, global_Y - mouse_velocity);
+                //}
+                //if (AKey.IsPressed)
+                //{
+                //    SetCursorPos(global_X - mouse_velocity, global_Y);
+                //}
+                //if (SKey.IsPressed)
+                //{
+                //    SetCursorPos(global_X, global_Y + mouse_velocity);
+                //}
+                //if (DLKey.IsPressed)
+                //{
+                //    SetCursorPos(global_X + mouse_velocity, global_Y);
+                //}
+            
+
+            return CallNextHookEx(m_HookHandle, nCode, wParam, lParam);
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try { mouse_velocity = int.Parse(textBox1.Text); }
+            catch
+            {
+                MessageBox.Show("格子要填int");
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            mouse_x.Text = System.Windows.Forms.Cursor.Position.X.ToString();
+            mouse_y.Text = System.Windows.Forms.Cursor.Position.Y.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string input;
+            input = script.Text;
+            string[] list = input.Split('\n');
+            for(int i = 0; i < list.Length; i++)
+            {
+                string[] inside = list[i].Split(' ');
+                if(inside[0] == "move")
+                {
+                    SetCursorPos(int.Parse(inside[1]), int.Parse(inside[2]));
+                }
+                else if(inside[0] == "wait")
+                {
+                    Thread.Sleep(int.Parse(inside[1])*1000*60);
+                }
+                else if(inside[0] == "click")
+                {
+                    Thread.Sleep(200);
+                    mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+                    mouse_event(MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+                }
+            }
+        }
+    }
+    public class KeyboardInfo
+    {
+        private KeyboardInfo() { }
+        [DllImport("user32")]
+        //GetKeyState為核心API 向windows要求鍵盤狀態以數值輸出 放入案鍵編號 若輸出 > 0 表示已按下
+        //要轉high low 值得知意義  -> high = 1 表示被按下 -> low = 1 表示被觸發  *轉換可直接抄
+        //https://baike.baidu.com/item/GetKeyState
+        private static extern short GetKeyState(int vKey);
+        public static KeyStateInfo GetKeyState(Keys key)
+        {
+            int vkey = (int)key;
+            short keyState = GetKeyState(vkey);
+            int low = Low(keyState);
+            int high = High(keyState);
+            bool toggled = (low == 1);
+            bool pressed = (high == 1);
+            return new KeyStateInfo(key, pressed, toggled);
+        }
+        private static int High(int keyState)
+        {
+            if (keyState > 0)
+            {
+                return keyState >> 0x10;
+            }
+            else
+            {
+                return (keyState >> 0x10) & 0x1;
+            }
+        }
+        private static int Low(int keyState)
+        {
+            return keyState & 0xffff;
+        }
+    }
+    public struct KeyStateInfo
+    {
+        //只是一個儲存一個按鍵 按下 觸發 狀態的結構
+        Keys m_Key;
+        bool m_IsPressed;
+        bool m_IsToggled;
+        public KeyStateInfo(Keys key, bool ispressed, bool istoggled)
+        {
+            m_Key = key;
+            m_IsPressed = ispressed;
+            m_IsToggled = istoggled;
+        }
+        public static KeyStateInfo Default
+        {
+            get
+            {
+                return new KeyStateInfo(Keys.None, false, false);
+            }
+        }
+        public Keys Key
+        {
+            get { return m_Key; }
+        }
+        public bool IsPressed
+        {
+            get { return m_IsPressed; }
+        }
+        public bool IsToggled
+        {
+            get { return m_IsToggled; }
+        }
+    }
+}
